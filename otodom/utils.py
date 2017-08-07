52ab54efdc9895e743a6dc7c78d6a92b6a99d35a
Jan Baryla <jan.baryla@limebrains.com>
2017-08-07 13:05:50 +0200

    Refactoring

--- otodom/utils.py
+++ otodom/utils.py
@@ -76,19 +76,19 @@ def get_region_from_filters(filters):
     return region_dict
 
 
-def _float(number):
-    return get_number_from_string(number, float)
+def _float(number, default=None):
+    return get_number_from_string(number, float, default)
 
 
-def _int(number):
-    return get_number_from_string(number, int)
+def _int(number, default=None):
+    return get_number_from_string(number, int, default)
 
 
-def get_number_from_string(s, number_type):
+def get_number_from_string(s, number_type, default):
     try:
         return number_type(s.replace(",", "."))
     except ValueError:
-        return s if s else None
+        return default
 
 
 def get_url(main_category, detail_category, region, ads_per_page="", page=None, **filters):
